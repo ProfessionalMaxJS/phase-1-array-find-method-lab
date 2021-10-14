3abe7a59392b6c48e74a1b6fd3485b8541e87548
Max <max.edison@yahoo.com>
2021-10-14 11:24:10 -0400

    commit tree 65619e8831b02b5e7ed4c8fcb7352ff3b5d2f9f0

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function superbowlWin(record){
+    function goodYear(element){
+    return element.result ==="W";
+    } 
+    if (record.find(goodYear)){
+    let W = record.find(goodYear);
+    return W.year;
+    }
+
+   
+}
+
+
+
 
